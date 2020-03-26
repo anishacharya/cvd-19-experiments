62e67733946f90174ac9243d732d4b96343e90eb
Anish Acharya <anishacharya@utexas.edu>
2020-03-26 15:56:23 -0500

    +

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 curr_dir = os.path.dirname(__file__)
 data_dir = curr_dir + '/../COVID-19/csse_covid_19_data/csse_covid_19_time_series/'
 
-confirmed_covid_jhu_time_series = data_dir + 'time_series_19-covid-Confirmed.csv'
-deaths_covid_jhu_time_series = data_dir + 'time_series_19-covid-Deaths.csv'
-recovered_covid_jhu_time_series = data_dir + 'time_series_19-covid-Recovered.csv'
+confirmed_covid_jhu_time_series = data_dir + 'time_series_covid19_recovered_global.csv'
+deaths_covid_jhu_time_series = data_dir + 'time_series_covid19_deaths_global.csv'
+recovered_covid_jhu_time_series = data_dir + ' time_series_covid19_recovered_global.csv'
